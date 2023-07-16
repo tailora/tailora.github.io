f8ddd2c6aece97348abde752b07f8ba77cf6de97
Can Celik <celik@me.com>
2023-07-16 17:13:08 +0300

    Update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Tailora",
-  base: "/docs/",
+  base: "/",
   description: "Application scaffolding for Laravel",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
